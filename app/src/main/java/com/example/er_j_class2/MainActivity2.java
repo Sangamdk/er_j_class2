8f5f54700f9ef21b7e454302ec20ec5d222c73c1
Sangamdk <Sangamdk@hotmail.com>
2024-09-19 19:41:52 +0530

    19 sept 2nd commite

--- app/src/main/java/com/example/er_j_class2/MainActivity2.java
+++ app/src/main/java/com/example/er_j_class2/MainActivity2.java
@@ -1,10 +1,12 @@
 package com.example.er_j_class2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,7 +19,7 @@ public class MainActivity2 extends AppCompatActivity {
 
     private EditText editTextNumber1;
     private EditText editTextNumber2;
-    private Button buttonAdd;
+    private Button buttonAdd,next;
     private TextView textViewResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,7 @@ public class MainActivity2 extends AppCompatActivity {
         editTextNumber1 = findViewById(R.id.firstnumber);
         editTextNumber2 = findViewById(R.id.secondnumber);
         buttonAdd = findViewById(R.id.submitbtn);
+        next=findViewById(R.id.next);
         textViewResult = findViewById(R.id.resultstxtview);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -47,5 +50,16 @@ public class MainActivity2 extends AppCompatActivity {
             textViewResult.getVisibility();
         }
     });
+
+    next.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    startActivity(new Intent(MainActivity2.this, MainActivity3.class));
+                                   startActivity(new Intent(MainActivity2.this, MainActivity3.class));
+                                    Toast.makeText(getApplicationContext(),"Hello ", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+    );
     }
 }
